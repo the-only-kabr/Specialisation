fa69b9cb8577a4001364708d7608e344e2bdf27a
zlokabr <zlokabr@gmail.com>
2022-11-20 14:45:15 +0300

    some files are added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Globalization;
+
+string text = Console.ReadLine();
+string[] words = text.Split(' ');
+string[] shortwords = new string[words.Length];
+int k = 0;
+ for (int i = 0; i < words.Length; i++)
+       {if (words[i].Length <= 3)
+             {
+        shortwords[k] = words[i];
+        k++;
+              }
+             
+        } 
+ for (int i = 0; i < k; i++)
+    Console.WriteLine(shortwords[i] + " ");
